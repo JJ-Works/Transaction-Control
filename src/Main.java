236dc8b60c60499757f2651e947375cf17396dbd
JJ <janarjanrb@gmail.com>
2025-06-15 15:57:55 +0545

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+
 public class Main {
+    public static void main(String[] args) {
+        LocalDate local = LocalDate.now();
+        System.out.println(local);
+        int dayOfMonth = local.getDayOfMonth();
+        String month = String.valueOf(local.getMonth());
+        int monthValue = local.getMonthValue();
+        int year = local.getYear();
+
+        System.out.println(dayOfMonth);
+        System.out.println(monthValue);
+        System.out.println(month);
+        System.out.println(year);
+    }
 }
