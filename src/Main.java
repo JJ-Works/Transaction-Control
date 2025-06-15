8d9fe99616c596b366bd4e37a5772a9755d58151
JJ <janarjanrb@gmail.com>
2025-06-15 16:28:10 +0545

    third

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate local = LocalDate.now();
-        System.out.println(local);
-        int dayOfMonth = local.getDayOfMonth();
-        String month = String.valueOf(local.getMonth());
-        int monthValue = local.getMonthValue();
-        int year = local.getYear();
-
-        System.out.println(dayOfMonth);
-        System.out.println(monthValue);
-        System.out.println(month);
-        System.out.println(year);
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
     }
 }
