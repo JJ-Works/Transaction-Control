75d5d0f09d897b66980d7170e06e1ee1c9fcaa86
JJ <janarjanrb@gmail.com>
2025-06-16 17:01:43 +0545

    first of the day

--- src/Main.java
+++ src/Main.java
@@ -20,9 +20,18 @@ public class Main{
 
             Scanner sc = new Scanner(System.in);
 
-            int firstAccount = sc.nextInt();
-            int secondAccount = sc.nextInt();
-            double newBalance = sc.nextDouble();
+            System.out.println("Do you want to ADD new Account? (Y/N)");
+            String choice = sc.nextLine().toLowerCase();
+
+            if (choice.equals("Y")){
+                System.out.println("Creating Account please wait!");
+                addUser(connection);
+            }
+
+
+            int senderAccount = sc.nextInt();
+            int receiverAccount = sc.nextInt();
+            double transferBalance = sc.nextDouble();
 
 // I don't know if I should add new user from here or using command line I think I should use command line first.
             String addUserQuery = "Insert INTO accountDetails(account_number, date, time, status) VALUES(101,CURDATE(),CURTIME(),'COMMITTED')";
@@ -48,6 +57,40 @@ public class Main{
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        finally {
+            sc.close();
+        }
+    }
+
+    static void addUser(Connection connection){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account Number: ");
+        int accountNum = sc.nextInt();
+
+
+
+        try {
+            String addUserQuery = "Insert INTO accountDetails(account_number, date, time, status) VALUES(?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(addUserQuery);
+            preparedStatement.setInt(1, accountNum);
+            preparedStatement.setDate(2,CURDATE());
+            preparedStatement.setTime(3,CURTIME());
+            preparedStatement.setString("COMMITTED");
+
+            int val = preparedStatement.executeUpdate();
+            if(val > 0){
+                System.out.println("Account Created!");
+            }else {
+                System.out.println("Something went wrong!");
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
